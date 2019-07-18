70450448081c264d92a6c8072b2d97b474f7a1db
efty <nj39581@gmail.com>
2019-07-18 00:47:07 -0700

    write addition of two numbers

---
+++ addition.java
@@ -0,0 +1,15 @@
+
+
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int a = -65;  
+	        int b = -135;  
+	        // It will return the sum of a and b.  
+	        System.out.println("The sum of a and b is = " + Integer.sum(a, b));  
+	    }  
+	}
+Type a 
